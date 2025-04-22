692dbe7809d7a74cd7f9d29a3ef86e53b1b2799e
knowave <kwaktaemin98@gmail.com>
2025-04-22 09:57:29 +0900

    lambda 실행 파일

---
+++ src/lambda.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+import { AppModule } from './app.module';
+import express from 'express';
+import serverlessExpress from '@codegenie/serverless-express';
+
+let cachedHandler: Handler;
+
+async function bootstrap(): Promise<Handler> {
+    const expressApp = express();
+    const adapter = new ExpressAdapter(expressApp);
+    const app = await NestFactory.create(AppModule, adapter);
+
+    await app.init();
+
+    return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+    if (!cachedHandler) cachedHandler = await bootstrap();
+    return cachedHandler(event, context, callback);
+};
